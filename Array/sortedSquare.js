0eb5c5d9fbb204dce71a96ba9020de1048544da2
IshaTHub <ishatiwari0508@gmail.com>
2025-07-04 23:30:12 +0530

    sortedSquare

---
+++ Array/sortedSquare.js
@@ -0,0 +1,25 @@
+function sortedSquares(nums) {
+    // Your code here
+    const result = new Array(nums.length);
+    let left = 0;
+    let right = nums.length - 1;
+    let position = nums.length - 1;
+    
+    while (left <= right) {
+        const leftSquare = nums[left] * nums[left];
+        const rightSquare = nums[right] * nums[right];
+        
+        if (leftSquare > rightSquare) {
+            result[position] = leftSquare;
+            left++;
+        } else {
+            result[position] = rightSquare;
+            right--;
+        }
+        position--;
+    }
+    
+    return result;
+}
+
+console.log(sortedSquares([-4, -1, 0, 3, 10])); // [0, 1, 9, 16, 100]
