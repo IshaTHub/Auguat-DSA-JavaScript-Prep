96fb082b0fb1b2852bc8203e100bfffbf2ab559e
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-08-01 23:24:21 +0530

    PascalTriangle.js
    118. Pascal's Triangle
    https://leetcode.com/problems/pascals-triangle/submissions/1719831999/?envType=daily-question&envId=2025-08-01

---
+++ LeetCode-Daily-Question/PascalTriangle.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const result = [];
+
+    for (let i = 0; i < numRows; i++) {
+        const row = [];
+        for (let j = 0; j <= i; j++) {
+            // First and last element of each row is 1
+            if (j === 0 || j === i) {
+                row.push(1);
+            } else {
+                // Inner elements are sum of two values above
+                row.push(result[i - 1][j - 1] + result[i - 1][j]);
+            }
+        }
+        result.push(row);
+    }
+
+    return result;
+};
