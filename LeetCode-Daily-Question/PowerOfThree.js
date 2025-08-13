d1b95dd44dd4b23367e986a889c150e2c2f3e5c8
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-08-13 23:04:13 +0530

    326. Power of Three
    https://leetcode.com/problems/power-of-three/?envType=daily-question&envId=2025-08-13

---
+++ LeetCode-Daily-Question/PowerOfThree.js
@@ -0,0 +1,7 @@
+var isPowerOfThree = function(n) {
+    if (n <= 0) return false; // negative numbers & zero can't be powers of 3
+    while (n % 3 === 0) {
+        n /= 3;
+    }
+    return n === 1;
+};
