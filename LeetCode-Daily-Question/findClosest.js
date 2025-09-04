3479046b75ce7298224b18209ba23df82c531c58
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-09-04 22:27:37 +0530

    3516. Find Closest Person
    https://leetcode.com/problems/find-closest-person/description/?envType=daily-question&envId=2025-09-04

---
+++ LeetCode-Daily-Question/findClosest.js
@@ -0,0 +1,12 @@
+var findClosest = function(x, y, z) {
+    const dist1 = Math.abs(x - z);
+    const dist2 = Math.abs(y - z);
+
+    if (dist1 < dist2) {
+        return 1;
+    } else if (dist2 < dist1) {
+        return 2;
+    } else {
+        return 0;
+    }
+};
