de2c6ee07c7f83bc4f97792e9bb25eb5ff5ef2bd
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-09-13 22:07:25 +0530

    3227. Vowels Game in a String
    https://leetcode.com/problems/vowels-game-in-a-string/description/?envType=daily-question&envId=2025-09-12

---
+++ LeetCode-Daily-Question/doesAliceWin.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var doesAliceWin = function(s) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    for (let ch of s) {
+        if (vowels.has(ch)) {
+            return true; 
+        }
+    }
+    return false; 
+};
