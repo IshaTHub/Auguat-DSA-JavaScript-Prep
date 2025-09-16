a27bb98be6a8e9db8a4894c6395293ba9820026a
IshaTHub <ishatiwari0508@gmail.com>
2025-09-16 23:44:21 +0530

    maxArea.js

---
+++ Array/twoPointerTechnique/maxArea.js
@@ -0,0 +1,22 @@
+function maxArea(height){
+    let left  = 0;
+    let right = height.length-1;
+    let maxWater = 0;
+    while(left < right){
+        const width = right - left;
+        const water = width * Math.min(height[left], height[right]);
+        maxWater = Math.max(maxWater, water);
+
+        if(height[left] < height[right] ){
+            left++
+        }
+        else{
+            right--;
+        }
+
+    }
+    return maxWater;
+}
+
+let height = [2,3,4,5,8,6,4,5];
+
