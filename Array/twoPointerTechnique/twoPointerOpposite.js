b137351052083ad3bfa75a04cf4108f540a3750d
IshaT-Hub <ishatiwari0508@gmail.com>
2025-09-16 14:50:46 +0530

    updated twoPointerOpposite

---
+++ Array/twoPointerTechnique/twoPointerOpposite.js
@@ -0,0 +1,22 @@
+//two pointer technique
+// Opposite direction and Sorted Array
+// two sum, pair with target, palindrome, reverse
+// O(n)
+
+function twoPointerOpposite(arr, target){
+    let start = 0;
+    let end  = arr.length-1;
+    while(start < end){
+        const sum  = arr[start] + arr[end]
+        if(sum === target){
+            return [start,end];
+        }
+        else if(sum < target){
+            start++;
+        }
+        else{
+            end--;
+        }
+    }
+}
+
