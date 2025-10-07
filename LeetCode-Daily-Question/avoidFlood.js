5f3fd96875d1a3312d5f4adfaf74c07d15079189
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-10-07 23:22:52 +0530

    1488. Avoid Flood in The City
    https://leetcode.com/problems/avoid-flood-in-the-city/description/?envType=daily-question&envId=2025-10-07

---
+++ LeetCode-Daily-Question/avoidFlood.js
@@ -0,0 +1,40 @@
+var avoidFlood = function(rains) {
+    const n = rains.length;
+    const ans = Array(n).fill(1);
+    const fullLakes = new Map(); // lake -> day it became full
+    const dryDays = []; // indices where rains[i] == 0
+    
+    for (let i = 0; i < n; i++) {
+        if (rains[i] > 0) {
+            const lake = rains[i];
+            ans[i] = -1;
+            
+            // If lake is already full, we need to dry it before this day
+            if (fullLakes.has(lake)) {
+                const prevDay = fullLakes.get(lake);
+                
+                // Find a dry day between prevDay and current day
+                let foundDryDay = false;
+                for (let j = 0; j < dryDays.length; j++) {
+                    if (dryDays[j] > prevDay && dryDays[j] < i) {
+                        ans[dryDays[j]] = lake;
+                        dryDays.splice(j, 1); // Remove used dry day
+                        foundDryDay = true;
+                        break;
+                    }
+                }
+                
+                if (!foundDryDay) {
+                    return []; // Flood is inevitable
+                }
+            }
+            
+            fullLakes.set(lake, i);
+        } else {
+            // Dry day - save it for later use
+            dryDays.push(i);
+        }
+    }
+    
+    return ans;
+};
