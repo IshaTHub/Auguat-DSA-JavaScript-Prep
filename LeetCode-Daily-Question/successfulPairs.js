512b84a64e898dcefa35ed2aafa63be29af9a0d2
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-10-08 22:01:52 +0530

    2300. Successful Pairs of Spells and Potions
    https://leetcode.com/problems/successful-pairs-of-spells-and-potions/description/?envType=daily-question&envId=2025-10-08

---
+++ LeetCode-Daily-Question/successfulPairs.js
@@ -0,0 +1,24 @@
+var successfulPairs = function(spells, potions, success) {
+    potions.sort((a, b) => a - b);
+    const m = potions.length;
+    const result = [];
+    
+    for (const spell of spells) {
+        const minPotion = Math.ceil(success / spell);
+        
+        // Binary search for first potion >= minPotion
+        let left = 0, right = m;
+        while (left < right) {
+            const mid = Math.floor((left + right) / 2);
+            if (potions[mid] >= minPotion) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        result.push(m - left);
+    }
+    
+    return result;
+};
