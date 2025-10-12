3c118e973be4436667c7ebb4a0d9431ae21b8846
Isha Tiwari <85096427+IshaTHub@users.noreply.github.com>
2025-10-13 00:00:09 +0530

    Update ReorderPowerofTwo.js

--- LeetCode-Daily-Question/ReorderPowerofTwo.js
+++ LeetCode-Daily-Question/ReorderPowerofTwo.js
@@ -8,7 +8,7 @@ var reorderedPowerOf2 = function(n) {
     const target = sorted(String(n));
     const set = new Set();
     
-    // generate powers of two up to 2^30
+    
     for (let i = 0; i <= 30; i++) {
         set.add(sorted(String(1 << i))); // 1<<i is 2^i for i<=30
     }
